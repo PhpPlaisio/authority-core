577d1a8e23239ef9c9d68fd16f47a9afa512a480
Paul Water <p.r.water@setbased.nl>
2018-06-14 19:58:46 +0200

    Use PHP 7.1 type declarations.

--- src/CoreAuthority.php
+++ src/CoreAuthority.php
@@ -1,5 +1,5 @@
 <?php
-//----------------------------------------------------------------------------------------------------------------------
+
 namespace SetBased\Abc\Authority;
 
 use SetBased\Abc\Abc;
@@ -20,7 +20,7 @@ class CoreAuthority implements Authority
    * @since 1.0.0
    * @api
    */
-  public function hasAccessToPage($pagId)
+  public function hasAccessToPage(int $pagId): bool
   {
     return !empty(Abc::$DL->abcAuthGetPageAuth(Abc::$companyResolver->getCmpId(), Abc::$session->getProId(), $pagId));
   }
@@ -32,10 +32,12 @@ class CoreAuthority implements Authority
    * @param int $usrId The ID of the user.
    * @param int $rolId The ID of the role.
    *
+   * @return void
+   *
    * @since 1.0.0
    * @api
    */
-  public function userGrantRole($usrId, $rolId)
+  public function userGrantRole(int $usrId, int $rolId): void
   {
     Abc::$DL->abcUserRoleGrantRole(Abc::$companyResolver->getCmpId(), $usrId, $rolId, date('Y-m-d'), '9999-12-31');
     Abc::$DL->abcProfileProperUser(Abc::$companyResolver->getCmpId(), $usrId);
@@ -48,10 +50,12 @@ class CoreAuthority implements Authority
    * @param int $usrId The ID of the user.
    * @param int $rolId The ID of the role.
    *
+   * @return void
+   *
    * @since 1.0.0
    * @api
    */
-  public function userRevokeRole($usrId, $rolId)
+  public function userRevokeRole(int $usrId, int $rolId): void
   {
     Abc::$DL->abcUserRoleRevokeRole(Abc::$companyResolver->getCmpId(), $usrId, $rolId);
     Abc::$DL->abcProfileProperUser(Abc::$companyResolver->getCmpId(), $usrId);
@@ -64,10 +68,12 @@ class CoreAuthority implements Authority
    * @param int $usrId The ID of the user.
    * @param int $rlgId The ID of the role group.
    *
+   * @return void
+   *
    * @since 1.0.0
    * @api
    */
-  public function userRevokeRoleGroup($usrId, $rlgId)
+  public function userRevokeRoleGroup(int $usrId, int $rlgId): void
   {
     $roles = Abc::$DL->abcSystemRoleGroupGetRoles($rlgId);
     foreach ($roles as $role)
